ea227d35b9de8abfc4a081f19668c815f4162a37
cjz9032 <402821051@qq.com>
2017-06-14 12:28:43 +0800

    发布

---
+++ app.js
@@ -0,0 +1,46 @@
+var express = require('express')
+var app = express();
+
+var http = require('http');
+var port = 8081;
+app.set('port', port);
+var server = http.createServer(app);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+app.use('/index.html',express.static('./index.html'));
+app.use('/dist',express.static('./dist'));
+app.use('/src',express.static('./src'));
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  console.log('Listening on ' + bind);
+}
